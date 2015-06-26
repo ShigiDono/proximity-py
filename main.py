33db659e0d2d2b8243a539fb46681f1e399bf40b
Petr Khrapchenkov <shigidono@gmail.com>
2015-06-26 14:40:28 +0900

    Separate graphs from 3 different ADC measurements

--- main.py
+++ main.py
@@ -10,16 +10,18 @@ from pyqtgraph.Qt import QtCore, QtGui
 win = pg.GraphicsWindow()
 win.setWindowTitle('pyqtgraph example: Scrolling Plots')
 
-p1 = win.addPlot()
-p2 = win.addPlot()
+p11 = win.addPlot()
+p12 = win.addPlot()
+p13 = win.addPlot()
 win.nextRow()
+p2 = win.addPlot()
 p3 = win.addPlot()
 p4 = win.addPlot()
-p1.setLabel('bottom', 'Time', 's')
+p11.setLabel('bottom', 'Time', 's')
 p2.setLabel('bottom', 'Time', 's')
 p3.setLabel('bottom', 'Time', 's')
 p4.setLabel('bottom', 'Time', 's')
-p1.setLabel('left', 'Proximity', '?')
+p11.setLabel('left', 'Proximity', '?')
 p2.setLabel('left', 'IR Light', '?')
 p3.setLabel('left', 'Visible light', '?')
 p4.setLabel('left', 'UV Index', '?')
@@ -33,9 +35,9 @@ data4 = [0] * 1000
 
 window_size, poly_order = 101, 3
 
-curve11 = p1.plot(data11, pen = pg.mkPen(color=(255, 0, 0)))
-curve12 = p1.plot(data12, pen = pg.mkPen(color=(0, 255, 0)))
-curve13 = p1.plot(data13, pen = pg.mkPen(color=(0, 0, 255)))
+curve11 = p11.plot(data11, pen = pg.mkPen(color=(255, 0, 0)))
+curve12 = p12.plot(data12, pen = pg.mkPen(color=(0, 255, 0)))
+curve13 = p13.plot(data13, pen = pg.mkPen(color=(0, 0, 255)))
 curve2 = p2.plot(data2, pen = pg.mkPen(color=(200, 50, 50)))
 curve3 = p3.plot(data3, pen = pg.mkPen(color=(100, 100, 255)))
 curve4 = p4.plot(data4, pen = pg.mkPen(color=(200, 50, 255)))
@@ -56,11 +58,11 @@ def update_data():
 		#print (numbers)
 		if len(numbers) != 7:
 			continue
-		data11.append(numbers[4])
+		data11.append(1.0/numbers[4])
 		del data11[0]
 		data12.append(numbers[5])
 		del data12[0]
-		data13.append(numbers[6])
+		data13.append(1.0/numbers[6])
 		del data13[0]
 
 		data2.append(numbers[3])
